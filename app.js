243250c684805084e69b829a584651f44d5d70a2
Matthew Tran <mvtran@scu.edu>
2023-08-31 00:20:55 -0700

    Aug31

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-// logoSpan has all of the 
+// "software"
+let softwareLogoSpan = document.querySelectorAll('.Software-logo');
+// "creative"
+let creativeLogoSpan = document.querySelectorAll('.Creative-logo');
+
 
 window.addEventListener('DOMContentLoaded',()=>{
-    
+   // console.log(logoSpan);
+
     setTimeout(()=>{
-        
-        logoSpan.forEach((span,i)=>{
-            setTimeout(()=>
-            {
-                span.classList.add('active');
-            },(i + 1) * 400)
-        });
-
-        setTimeout(()=>{
-            logoSpan.forEach((span,i)=>{
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
+            // software
+            softwareLogoSpan.forEach((span,i)=>{
+                setTimeout(()=>
+                {
+                    span.classList.add('active');
+                },(i + 1) * 400)
+            });
+            setTimeout(()=>{
+                softwareLogoSpan.forEach((span,i)=>{
+                    setTimeout(()=>{
+                        span.classList.remove('active');
+                        span.classList.add('fade');
+                    })
                 })
-            })
-        },2000);
-        setTimeout(()=>{
-            intro.style.top = '-100vh';
-        },2300)
+            },2000);
 
+
+            //creative
+            
+            creativeLogoSpan.forEach((span,i)=>{
+                setTimeout(()=>
+                {
+                    span.classList.add('active');
+                },(i + 1) * 400 + 2000)
+            });
+            /*
+            setTimeout(()=>{
+                creativeLogoSpan.forEach((span,i)=>{
+                    setTimeout(()=>{
+                        span.classList.remove('active');
+                        span.classList.add('fade');
+                    })
+                })
+            },4500);
+            */
+            // moves the whole intro sequence up
+            setTimeout(()=>{
+                intro.style.top = '-100vh';
+            },8000)
+        
+        
     })
+
 
     
     
